36bfdd859c8c89bd35ddd88a8133d3c8f73932c9
Ode1l <ljhnevergiveup@gmail.com>
2023-01-27 18:06:12 +0800

    modify navbar

--- sw.js
+++ sw.js
@@ -14,6 +14,7 @@ const CACHE_NAMESPACE = 'main-'
 const CACHE = CACHE_NAMESPACE + 'precache-then-runtime';
 const PRECACHE_LIST = [
   "./",
+  "./offline.html",
   "./js/jquery.min.js",
   "./js/bootstrap.min.js",
   "./js/hux-blog.min.js",
